49ba1f57efb396bab84d3c27a7a010fb0bbbb5a8
Lukhz <lukas-luebberding@hotmail.de>
2022-09-02 14:50:22 +0200

    final_frontend

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,7 +1,5 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
-import { Capacitor } from '@capacitor/core';
-import { GoogleMap } from '@capacitor/google-maps';
-import { CapacitorGoogleMaps } from '@capacitor/google-maps/dist/typings/implementation';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-tab2',
@@ -11,49 +9,7 @@ import { CapacitorGoogleMaps } from '@capacitor/google-maps/dist/typings/impleme
 
 export class Tab2Page {
 
-  //@ViewChild('map') mapView: ElementRef;
-  @ViewChild('map')
-  mapRef: ElementRef<HTMLElement>;
-  newMap: GoogleMap;
-
   constructor() {}
  
-  async createMap() {
-    this.newMap = await GoogleMap.create({
-      id: 'map',
-      element: this.mapRef.nativeElement,
-      apiKey: "AIzaSyAjOGgmZTAyT9mYOGo14b4W6rMOhXOl9n0",
-      config: {
-        center: {
-          lat: 33.6,
-          lng: -117.9,
-        },
-        zoom: 8,
-      },
-    });
-  }
- 
- 
-  /*
-  ionViewDidEnter(){
-    this.createMap();
-  }
-
-  createMap(){
-    const boundingRect = this.mapView.nativeElement.getBoundingClientRect() as DOMRect;
-    console.log("file:tab2.page.ts",boundingRect)
-
-    CapacitorGoogleMaps.create({
-      width: Math.round(boundingRect.width),
-      height: Math.round(boundingRect.height),
-      x: Math.round(boundingRect.x),
-      y: Math.round(boundingRect.y),
-      //latitude:
-      //longitude:
-      zoom: 3,
-      //liteMode:
-      })
-    }*/
-
 }
 
