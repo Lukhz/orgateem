e7f82f2e96bdacb40306cc11440b133deb48d1e9
fianteh011 <hafiyyan98@gmail.com>
2022-09-02 19:50:50 +0200

    Navigation und Routing gefixt

--- src/app/tab1/tab1-routing.module.ts
+++ src/app/tab1/tab1-routing.module.ts
@@ -1,21 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventDetailsPage } from '../event-details/event-details.page';
 import { Tab1Page } from './tab1.page';
 
 const routes: Routes = [
   {
     path: '',
     component: Tab1Page,
-
   },
   {
     path: 'event-details',
-    loadChildren: () => import('../event-details/event-details.module').then( m => m.EventDetailsPageModule)
+    component: EventDetailsPage,
   },
 ];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class Tab1PageRoutingModule {}
